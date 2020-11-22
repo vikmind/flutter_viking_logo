a35fbb1ba9d94d52afc1ca3526cae36bb1de2a49
Viktor Marchenko <viktor.marchenko@gmail.com>
2020-11-23 01:10:59 +0300

    Full picture, some animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'dart:typed_data';
+import 'dart:math';
 import 'dart:ui' as ui show Gradient;
 
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:svg_path_parser/svg_path_parser.dart';
 
 void main() {
   runApp(MyApp());
@@ -38,7 +36,26 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+    animation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,162 +63,177 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Opacity(
-              opacity: .2,
-              child: SvgPicture.asset(
-                'assets/logo.svg',
-                width: WIDTH,
-                height: HEIGHT,
-              ),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Theme.of(context).canvasColor,
             ),
-            SizedBox(
-              width: WIDTH,
+          ),
+          child: FadeTransition(
+            opacity: animation,
+            child: SizedBox(
+              width: 200,
+              height: 200,
               child: AspectRatio(
                 aspectRatio: WIDTH / HEIGHT,
                 child: CustomPaint(
                   size: Size(WIDTH, HEIGHT),
-                  painter: LogoPainter(),
+                  painter: LogoPainter(animation),
                 ),
               ),
             ),
-          ],
+          ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (controller.isCompleted) {
+            controller.reverse();
+          }
+          if (controller.isDismissed) {
+            controller.forward();
+          }
+        },
+        child: Icon(Icons.animation),
       ),
     );
   }
 }
 
 class LogoPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    print(size);
-    final lightPaint = Paint()..color = LIGHT_BLUE;
-    final darkPaint = Paint()..color = DARK_BLUE;
-    /// Left Stripe
-    /// m392.09 163.01
-    /// l186.31 186.32
-    /// l-514.76 514.76
-    /// c-23.32 23.32 -63.18 6.8 -63.18 -26.17
-    /// v-318.46
-    /// c0 -9.82 3.9 -19.23 10.84 -26.17
-    /// l330.28 -330.28
-    /// c13.95 -13.94 36.56 -13.94 50.51 0
-    Path leftStripe = parseSvgPath(
-        "m392.09 163.01 186.31 186.32-514.76 514.76c-23.32 23.32-63.18 6.8-63.18-26.17v-318.46c0-9.82 3.9-19.23 10.84-26.17l330.28-330.28c13.95-13.94 36.56-13.94 50.51 0z");
+  final Animation<double> animation;
+  LogoPainter(this.animation) : super(repaint: animation);
 
-    /// Right stripe
-    /// m340.67 163.93
-    /// l-185.4 185.4
-    /// l514.76 514.76
-    /// c23.32 23.32 63.18 6.8 63.18 -26.17
-    /// v-318.46
-    /// c0 -9.82 -3.9 -19.23 -10.84 -26.17
-    /// l-329.36 -329.36
-    /// c-14.46 -14.46 -37.89 -14.46 -52.34 0
-    Path rightStripe = parseSvgPath(
-        "m340.67 163.93-185.4 185.4 514.76 514.76c23.32 23.32 63.18 6.8 63.18-26.17v-318.46c0-9.82-3.9-19.23-10.84-26.17l-329.36-329.36c-14.46-14.46-37.89-14.46-52.34 0z");
+  Path _leftStripe;
+  Path get leftStripe {
+    if (_leftStripe != null) return Path.from(_leftStripe);
+    _leftStripe = Path();
+    _leftStripe.moveTo(392.09, 163.01);
+    _leftStripe.relativeLineTo(186.31, 186.32);
+    _leftStripe.relativeLineTo(-514.76, 514.76);
+    _leftStripe.relativeCubicTo(-23.32, 23.32, -63.18, 6.8, -63.18, -26.17);
+    _leftStripe.relativeLineTo(0, -318.46);
+    _leftStripe.relativeCubicTo(0, -9.82, 3.9, -19.23, 10.84, -26.17);
+    _leftStripe.relativeLineTo(330.28, -330.28);
+    _leftStripe.relativeCubicTo(13.95, -13.94, 36.56, -13.94, 50.51, 0);
+    return Path.from(_leftStripe);
+  }
 
-    /// Gradient
-    /// m366.84 560.89
-    /// l-211.57 -211.56
-    /// l 134.13 289
-    Path gradient = parseSvgPath("m366.84 560.89-211.57-211.56 134.13 289z");
+  Path _rightStripe;
+  Path get rightStripe {
+    if (_rightStripe != null) return Path.from(_rightStripe);
+    _rightStripe = Path();
+    _rightStripe.moveTo(340.67, 163.93);
+    _rightStripe.relativeLineTo(-185.4, 185.4);
+    _rightStripe.relativeLineTo(514.76, 514.76);
+    _rightStripe.relativeCubicTo(23.32, 23.32, 63.18, 6.8, 63.18, -26.17);
+    _rightStripe.relativeLineTo(0, -318.46);
+    _rightStripe.relativeCubicTo(0, -9.82, -3.9, -19.23, -10.84, -26.17);
+    _rightStripe.relativeLineTo(-329.36, -329.36);
+    _rightStripe.relativeCubicTo(-14.46, -14.46, -37.89, -14.46, -52.34, 0);
+    return Path.from(_rightStripe);
+  }
 
+  Path _gradient;
+  Path get gradient {
+    if (_gradient != null) return Path.from(_gradient);
+    _gradient = Path();
+    _gradient.moveTo(366.84, 560.89);
+    _gradient.relativeLineTo(-211.57, -211.56);
+    _gradient.relativeLineTo(134.13, 289);
+    return Path.from(_gradient);
+  }
+
+  Path _rightHorn;
+  Path get rightHorn {
+    if (_rightHorn != null) return Path.from(_rightHorn);
+    _rightHorn = Path();
+    _rightHorn.moveTo(603.9, 1.38);
+    _rightHorn.relativeCubicTo(0, 0, 59.85, 179.05, -14.77, 243.66);
+    _rightHorn.relativeCubicTo(0, 0, 86.5, 48.26, 108.35, 111.08);
+    _rightHorn.relativeCubicTo(-.01, 0, 126.09, -169.51, -93.58, -354.74);
+    return Path.from(_rightHorn);
+  }
+
+  Path _rightBase;
+  Path get rightBase {
+    if (_rightBase != null) return Path.from(_rightBase);
+    _rightBase = Path();
+    _rightBase.moveTo(661.55, 420.26);
+    _rightBase.relativeCubicTo(0, 0, 51.6, -27.58, 26.62, -72.4);
+    _rightBase.relativeCubicTo(-9, -26.62, -103.41, -107.25, -116.96, -102.05);
+    _rightBase.relativeCubicTo(0, 0, -54.9, 28.28, -54.9, 28.28);
+    _rightBase.relativeCubicTo(0, 0, 111.98, 70.1, 145.24, 146.17);
+    return Path.from(_rightBase);
+  }
+
+  Path _leftHorn;
+  Path get leftHorn {
+    if (_leftHorn != null) return Path.from(_leftHorn);
+    _leftHorn = Path();
+    _leftHorn.moveTo(129.66, .38);
+    _leftHorn.relativeCubicTo(0, 0, -59.85, 179.05, 14.77, 243.66);
+    _leftHorn.relativeCubicTo(0, 0, -86.5, 48.26, -108.35, 111.08);
+    _leftHorn.relativeCubicTo(0, 0, -126.1, -169.51, 93.58, -354.74);
+    return Path.from(_leftHorn);
+  }
+
+  Path _leftBase;
+  Path get leftBase {
+    if (_leftBase != null) return Path.from(_leftBase);
+    _leftBase = Path();
+    _leftBase = Path();
+    _leftBase.moveTo(72.02, 420.26);
+    _leftBase.relativeCubicTo(0, 0, -51.6, -27.58, -26.62, -72.4);
+    _leftBase.relativeCubicTo(3, -20, 103.41, -107.25, 116.96, -102.05);
+    _leftBase.relativeCubicTo(0, 0, 54.9, 28.28, 54.9, 28.28);
+    _leftBase.relativeCubicTo(0, 0, -111.98, 70.1, -145.24, 146.17);
+    return Path.from(_leftBase);
+  }
+
+  Paint get lightPaint => Paint()..color = LIGHT_BLUE;
+  Paint get darkPaint => Paint()..color = DARK_BLUE;
+
+  Paint get gradientPaint {
     final gradientColor =
         HSLColor.fromColor(Color(0xFF055799)).withLightness(.25);
-    final gradientPaint = Paint()
+    return Paint()
       ..shader = ui.Gradient.linear(
         Offset(128.7, 289.6),
         Offset(304, 429),
         [gradientColor.toColor(), Color(0xFF055799)],
       );
+  }
 
-    /// Right HORN
-    /// m603.9 1.38
-    /// s59.85 179.05 -14.77 243.66
-    /// c0 0 86.5 48.26 108.35 111.08
-    /// c-.01 0 126.09 -169.51 -93.58 -354.74
-    Path rightHorn = Path();
-    rightHorn.lineTo(603.9, 1.38);
-    rightHorn.cubicTo(
-      603.9,
-      1.38,
-      603.9 + 59.85,
-      1.38 + 179.05,
-      603.9 - 14.77,
-      1.38 + 243.66,
-    );
-    rightHorn.cubicTo(
-      603.9 - 14.77,
-      1.38 + 243.66,
-      603.9 - 14.77 + 86.5,
-      1.38 + 243.66 + 48.26,
-      603.9 - 14.77 + 108.35,
-      1.38 + 243.66 + 111.08,
-    );
-    rightHorn.cubicTo(
-      603.9 - 14.77 + 108.35-.01,
-      1.38 + 243.66 + 111.08,
-      603.9 - 14.77 + 108.35+ 126.09,
-      1.38 + 243.66 + 111.08-169.51,
-      603.9 - 14.77 + 108.35-93.58,
-      1.38 + 243.66 + 111.08-354.74,
-    );
+  @override
+  void paint(Canvas canvas, Size size) {
+    // Prepare
+    canvas.translate(121 * (size.width / WIDTH), 23 * (size.width / WIDTH));
+    canvas.scale(size.width / WIDTH);
 
-    /// Right base
-    /// m129.66.38
-    /// s-59.85 179.05 14.77 243.66
-    /// c0 0 -86.5 48.26 -108.35 111.08
-    /// c0 0 -126.1 -169.51 93.58-354.74
-    Path rightBase = Path();
+    // Animate
+    // canvas.scale(animation.value);
+    // canvas.rotate(pi*2*animation.value);
 
-    /// Left horn
-    /// m661.55 420.26
-    /// s51.6 -27.58 26.62 -72.4
-    /// s-103.41 -107.25 -116.96 -102.05
-    /// s-54.9 28.28 -54.9 28.28
-    /// s111.98 70.1 145.24 146.17
-    Path leftHorn = Path();
+    // Draw
+    canvas.drawPath(
+        leftStripe.shift(
+          Offset(
+            100 * (1 - animation.value),
+            100 * (1 - animation.value),
+          ),
+        ),
+        darkPaint);
+    canvas.drawPath(rightStripe,
+        lightPaint..color = LIGHT_BLUE.withOpacity(animation.value));
+    if (animation.value > 0.9) {
+      canvas.drawPath(gradient, gradientPaint);
+    }
 
-
-    /// Left base
-    /// m72.02 420.26
-    /// s-51.6 -27.58 -26.62 -72.4
-    /// s103.41 -107.25 116.96 -102.05
-    /// s54.9 28.28 54.9 28.28
-    /// s-111.98 70.1-145.24 146.17
-    Path leftBase = Path();
-
-    canvas.translate(121*(size.width/WIDTH), 23*(size.width/WIDTH));
-    canvas.transform(
-      Float64List.fromList([
-        1,
-        0,
-        0,
-        0,
-        0,
-        1,
-        0,
-        0,
-        0,
-        0,
-        1,
-        0,
-        0,
-        0,
-        0,
-        1 / (size.width / WIDTH),
-      ]),
-);
-    canvas.drawPath(leftStripe, darkPaint);
-    canvas.drawPath(rightStripe, lightPaint);
-    canvas.drawPath(gradient, gradientPaint);
     canvas.drawPath(rightHorn, lightPaint);
-    canvas.drawPath(leftHorn, darkPaint);
     canvas.drawPath(rightBase, darkPaint);
+
+    canvas.drawPath(leftHorn, darkPaint);
     canvas.drawPath(leftBase, lightPaint);
   }
 
